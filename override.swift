ed7af64f2f6b2ee136df634c7378631aece1b344
zasabri <zasabri@gmail.com>
2023-01-14 16:48:16 +0100

    zasabri

--- override.swift
+++ override.swift
@@ -1,27 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.swift                                         :+:      :+:    :+:   */
+/*   override.swift                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/14 16:41:59 by zasabri           #+#    #+#             */
-/*   Updated: 2023/01/14 16:42:00 by zasabri          ###   ########.fr       */
+/*   Created: 2023/01/14 16:43:25 by zasabri           #+#    #+#             */
+/*   Updated: 2023/01/14 16:47:58 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 import Cocoa
 
-struct test
+class superone
 {
-	let name: String
-	var num: Int
-	func agenda()
+	func whatClass()
 	{
-		print("name: \(name)\nnumber\(num)")
+		print("I'm super one")
 	}
 }
 
-let p1 = test(name: "zakaria",num: 0612608931)
-p1.agenda()
+class subone: superone
+{
+	override func whatClass()
+	{
+		print("I'm sub one")
+	}
+}
+
+var checker1 = subone()
+checker1.whatClass()
+var checker2 = superone()
+checker2.whatClass()
+
