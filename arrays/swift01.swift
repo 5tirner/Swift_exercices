9219f644e0789ee3a3a9a7d79810a6001f5de520
zasabri <zasabri@gmail.com>
2023-01-18 04:16:25 +0100

    zasabri

--- arrays/swift01.swift
+++ arrays/swift01.swift
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.swift                                         :+:      :+:    :+:   */
+/*   swift01.swift                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/14 16:41:59 by zasabri           #+#    #+#             */
-/*   Updated: 2023/01/14 16:42:00 by zasabri          ###   ########.fr       */
+/*   Created: 2023/01/18 04:15:25 by zasabri           #+#    #+#             */
+/*   Updated: 2023/01/18 04:15:26 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-import Cocoa
-
-struct test
-{
-	let name: String
-	var num: Int
-	func agenda()
-	{
-		print("name: \(name)\nnumber\(num)")
-	}
-}
-
-let p1 = test(name: "zakaria",num: 0612608931)
-p1.agenda()
